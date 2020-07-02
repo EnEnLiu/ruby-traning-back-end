32159a6cdfb59ee39bbed7f4ca4cb4f6978c1697
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-07-02 15:33:51 +0800

    add user model vallidates

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,11 @@
 class User < ApplicationRecord
   has_many :task
+
+  validates :name, presence: true
+  validates :email, presence: true, 
+                    uniqueness: true
+  validates :password, presence: true, 
+                       length: { in: 6..20 },
+                       confirmation: true
+  # message: "Password must be at least 8 characters long, at least 1 English letter" }
 end
