bd708009d2f53087b34d71a94c26e757554a3c75
LiuEnTzu <tainn_50554@yahoo.com.tw>
2020-07-03 14:10:49 +0800

    add Faker

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,17 @@ module Training5x
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
     config.i18n.default_locale = "zh-TW"
-
+    # config.generators do |g| 
+    #   g.test_framework :rspec,
+    #   g.fixture_replacement :factory_bot,
+    #   view_specs: false, 
+    #   helper_specs: false, 
+    #   routing_specs: false, 
+    #   request_specs: false
+    # end
+    config.generators do |g|
+      g.factory_bot false
+    end
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,59 +3,43 @@ require 'rails_helper'
 RSpec.describe User, type: :model do
   context '註冊成功' do
     it '所有欄位皆有內容，email不與其他用戶重複，密碼6個字以上' do
-      user = User.new(
-        name: 'nn',
-        email: 'nn@example.com.tw',
-        password: 'password',
-        password_confirmation: 'password'
-      )
-      expect(user).to be_valid
+      expect(FactoryBot.build(:user)).to be_valid
     end
   end
   context '註冊失敗' do
     it 'name欄位是空的' do
-      user = User.new(name: nil)
+      user = FactoryBot.build(:user, name: nil)
       user.valid?
       expect(user.errors[:name]).to include('不能為空白')
     end
     it 'name欄位內容字數不在1-10之間' do
-      user = User.new(name: '12345678910')
+      user = FactoryBot.build(:user, name: '12345678910')
       user.valid?
       expect(user.errors[:name]).to include('過長（最長是 10 個字）')
     end
     it 'email欄位是空的' do
-      user = User.new(email: nil)
+      user = FactoryBot.build(:user, email: nil)
       user.valid?
       expect(user.errors[:email]).to include('不能為空白')
     end
     it 'password欄位是空的' do
-      user = User.new(password: nil)
+      user = FactoryBot.build(:user, password: nil)
       user.valid?
       expect(user.errors[:password]).to include('不能為空白')
     end
     it 'password欄位內容字數不在6-20之間' do
-      user = User.new(password: 12345)
+      user = FactoryBot.build(:user, password: '12345')
       user.valid?
       expect(user.errors[:password]).to include('過短（最短是 6 個字）')
     end
     it 'email格式不符' do
-      user = User.new(email: '123.com')
+      user = FactoryBot.build(:user, email: '123.com')
       user.valid?
       expect(user.errors[:email]).to include('是無效的')
     end
     it 'email與其他用戶重複' do
-      User.create(
-        name: 'nn',
-        email: 'nn@example.com',
-        password: 'password',
-        password_confirmation: 'password'
-      )
-      user = User.new(
-        name: 'nnn',
-        email: 'nn@example.com',
-        password: 'password',
-        password_confirmation: 'password'
-      )
+      FactoryBot.create(:user, email: 'nn@example.com')
+      user = FactoryBot.build(:user, email: 'nn@example.com')
       user.valid?
       expect(user.errors[:email]).to include('已經被使用')
     end
